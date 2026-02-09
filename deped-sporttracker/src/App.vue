671c5185c278f205398d7ceec25aa27f6efcbd0a
kiichay <noynaykeiichi@gmail.com>
2026-02-09 13:37:32 +0800

    haluuu

--- deped-sporttracker/src/App.vue
+++ deped-sporttracker/src/App.vue
@@ -2,15 +2,17 @@
 import { ref } from 'vue'
 import HomeTab from './components/HomeTab.vue'
 import HomeLeagues from './components/HomeLeagues.vue'
+import AboutUs from './components/AboutUs.vue'
 
 const activeTab = ref('Home')
 </script>
 
 <template>
-    <div>
-      <HomeTab :active="activeTab" @change="activeTab = $event">
-        <!-- Content slot for cards below navigation -->
-        <HomeLeagues v-if="activeTab === 'Home'" />
-      </HomeTab>
+  <div>
+    <HomeTab v-if="activeTab !== 'About Us'" :active="activeTab" @change="activeTab = $event">
+      <!-- Content slot for cards below navigation -->
+      <HomeLeagues v-if="activeTab === 'Home'" />
+    </HomeTab>
+    <AboutUs v-if="activeTab === 'About Us'" :active="activeTab" @change="activeTab = $event" />
   </div>
 </template>
